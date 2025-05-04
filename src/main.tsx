726affb61be256462918b2d54b9f938f831d2ca7
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-05-04 14:23:59 +0530

    main fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import { AuthProvider } from "./contexts/authContext";
 import { BookingProvider } from "./contexts/bookingContext";
 import { DentistProvider } from "./contexts/dentistContext";
